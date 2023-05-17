d9da48167d79ae133bb22b6da55fbb315596c553
Mah <madavetto@gmail.com>
2023-05-17 17:34:29 -0300

    modified: index.html & js/main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,23 @@ const form = document.getElementById("novoItem");
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    
-    console.log(evento);
-    console.log(evento.target.elements['nome'].value);
-    console.log(evento.target.elements['quantidade'].value);
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
 
+function criaElemento(nome, quantidade) {
+    console.log(nome);
+    console.log(quantidade);
+
+    //<li class="item"><strong>7</strong>Camisas</li>
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem) 
+    numeroItem.innerHTML += nome
+
+
+    console.log(novoItem);
+}
