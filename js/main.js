d3858daaf66b986af20d4056961452f5a6608ff9
Mah <madavetto@gmail.com>
2023-05-19 23:57:22 -0300

    modified: js/main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista")
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //Declara um array de nome itens: //Lista vazia // parse para transformar em JavaScript
+
+console.log(itens)
+console.log([])
+
+itens.forEach( (elemento) => {
+    console.log(elemento)
+} )
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-
+    //Cria variáveis para acessar os valores enviados:
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
 
     nome.value = ""
     quantidade.value = ""
@@ -18,15 +27,25 @@ function criaElemento(nome, quantidade) {
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement("strong")
+    const numeroItem = document.createElement("strong") 
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem) 
+    
     numeroItem.innerHTML += nome
 
     lista.appendChild(novoItem)
 
-    localStorage.setItem('nome', nome)
-    localStorage.setItem('quantidade', quantidade)
+    //Transforme a variável já criada, itemAtual, em um objeto object que receba os valores de nome e quantidade, transforma estes valores em string em localStorage:
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    //Insire a variável itemAtual nesse array itens, utilizando o método push:
+    itens.push(itemAtual)
+
+     //transforma estes valores em string:
+    localStorage.setItem('itens', JSON.stringify(itens))
+   
 
 }
