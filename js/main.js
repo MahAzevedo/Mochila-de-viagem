10b780b6ebca4c32162b7747c8da95406ce69a98
Mah <madavetto@gmail.com>
2023-05-23 19:03:36 -0300

    modified: js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || []; //Declara um array de nome itens: //Lista vazia // JSON.parse para transformar em JavaScript
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //Declara um array de nome itens: //Lista vazia // JSON.parse para parsear,interpretar,decodificar em JavaScript
+/*
+Parse, parsear, o que seria:
+2. Parsear. Definição técnica utilizada por programadores. 
+Significa extrair conteúdos de um conjunto de caracteres (geralmente separados por ponto e virgula, pipe, chave ou colchete).
+Os possíveis significados são: "decodificar" ; "interpretar" e dependendo do caso "converter".
+*/
 
 console.log(itens)
 console.log([])
