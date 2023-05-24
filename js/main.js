0a36a5b59ee1334040609bdbac5ea919cb374be4
Mah <madavetto@gmail.com>
2023-05-24 00:07:44 -0300

    modified: index.html & js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || []; //Declara um array de nome itens: //Lista vazia // JSON.parse para parsear,interpretar,decodificar em JavaScript
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //Declara um array de nome itens: //Lista vazia // JSON.parse() para parsear,interpretar,decodificar em JavaScript
 /*
 Parse, parsear, o que seria:
 2. Parsear. Definição técnica utilizada por programadores. 
@@ -8,11 +8,9 @@ Significa extrair conteúdos de um conjunto de caracteres (geralmente separados 
 Os possíveis significados são: "decodificar" ; "interpretar" e dependendo do caso "converter".
 */
 
-console.log(itens)
-console.log([])
 
 itens.forEach( (elemento) => {
-    console.log(elemento)
+    criaElemento(elemento)
 } )
 
 form.addEventListener("submit", (evento) => {
@@ -22,36 +20,36 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
 
-    nome.value = ""
-    quantidade.value = ""
-
-})
-
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement("strong") 
-    numeroItem.innerHTML = quantidade
-    novoItem.appendChild(numeroItem) 
-    
-    numeroItem.innerHTML += nome
-
-    lista.appendChild(novoItem)
-
-    //Transforme a variável já criada, itemAtual, em um objeto object que receba os valores de nome e quantidade, transforma estes valores em string em localStorage:
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
+      //Transforme a variável já criada, itemAtual, em um objeto object que receba os valores de nome e quantidade, transforma estes valores em string em localStorage:
+     const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
+
+    criaElemento(itemAtual)
 
     //Insire a variável itemAtual nesse array itens, utilizando o método push:
     itens.push(itemAtual)
 
      //transforma estes valores em string:
     localStorage.setItem('itens', JSON.stringify(itens))
-   
+
+    nome.value = ""
+    quantidade.value = ""
+
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong") 
+    numeroItem.innerHTML = item.quantidade
+    novoItem.appendChild(numeroItem) 
+    
+    numeroItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
 
 }
